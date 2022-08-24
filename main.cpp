1eb76799f2bcc2af849d42c32d619dd416fc27b2
Nikolas Mikolaski <mikolaskin@gmail.com>
2022-08-24 11:59:02 -0400

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,54 @@
+# include<iostream>
+
+
+/************* TODO LIST *************/
+
+//function that creates a board
+void create_board()
+{
+
+}
+//function that prints the board
+void print_board()
+{
+
+}
+
+//function that gets move from player
+void get_move()
+{
+
+}
+//function that makes move on the board
+void make_move()
+{
+
+}
+
+//function that checks if move is legal (and if board is full)
+bool check_move()
+{
+
+}
+
+//function that checks win conditions
+
+
+void main()
+{
+	//loop until there is a winner or the board is full
+	//	print the board
+	//	
+	//	get move from player
+	// 
+	//	check if the move is legal
+	// 
+	//	make the move on the board
+	// 
+	//	check if move is winning
+	//		if it is winner, declare winner and break out of loop
+	//		if the board is full and no winner, declare a draw
+	// 
+	// 
+		
+}
