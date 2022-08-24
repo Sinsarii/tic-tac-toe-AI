0c45653622ed307007c53449b2c55c084d56341f
Nikolas Mikolaski <mikolaskin@gmail.com>
2022-08-24 12:31:25 -0400

    - create board can now make a new board with test values - print_board will print a board of any size

--- main.cpp
+++ main.cpp
@@ -1,17 +1,43 @@
-# include<iostream>
+#include <iostream>
+#include <vector>
+#include <string>
 
+using std::string;
+using std::vector;
+using std::cout;
+using std::cin;
+using namespace std;
 
 /************* TODO LIST *************/
 
 //function that creates a board
-void create_board()
+vector<vector<string>> new_board()
 {
+	string NONE = " ";
 
+	vector<vector<string>> board;
+
+	vector<string> l1 = { NONE, "O", NONE };
+	vector<string> l2 = { "X",NONE, "O" };
+	vector<string> l3 = { "X", "X", NONE };
+
+	board.push_back(l1);
+	board.push_back(l2);
+	board.push_back(l3);
+
+	return board;
 }
 //function that prints the board
-void print_board()
+void print_board(vector<vector<string>> & board)
 {
-
+	for (int i = 0; i < board.size(); i++)
+	{
+		for (int j = 0; j < board[i].size(); j++)
+		{
+			cout << board[i][j] << " ";
+		}
+		cout <<  endl;
+	}
 }
 
 //function that gets move from player
@@ -28,7 +54,7 @@ void make_move()
 //function that checks if move is legal (and if board is full)
 bool check_move()
 {
-
+	return true;
 }
 
 //function that checks win conditions
@@ -36,6 +62,10 @@ bool check_move()
 
 void main()
 {
+	vector<vector<string>> board = new_board();
+
+	print_board(board);
+
 	//loop until there is a winner or the board is full
 	//	print the board
 	//	
